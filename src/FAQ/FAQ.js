0314fd12869fc1d7090691b7d96609b57da92771
Stefan0305 <ni.stefan.95@gmail.com>
2023-03-02 00:43:26 +0100

    faq section layout created, styles added

---
+++ src/FAQ/FAQ.js
@@ -0,0 +1,17 @@
+class FAQ extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadow.innerHTML = `
+        <link rel="stylesheet" href="../../style.css">`;
+  }
+}
+
+customElements.define("faq-section", FAQ);
